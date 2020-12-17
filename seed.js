b1bdb1c71adbb2de149572748ac7bd6fcb82a2a5
aprilyanggarwood <yangyyj822@hotmail.com>
2020-12-16 18:26:00 -0800

    built sequelize orm , handlebar , router controller for menu page

---
+++ seed.js
@@ -0,0 +1,38 @@
+const db = require("./models");
+const seedData = [
+  {
+    name: "Cappucino",
+    price_sm: 3.5,
+    price_md: 5.5,
+    price_lg: 7,
+    coffee_type: "hot",
+    img_url:
+      "https://www.caffesociety.co.uk/assets/recipe-images/cappuccino-small.jpg",
+  },
+  {
+    name: "Frappucino",
+    price_sm: 4.5,
+    price_md: 6.5,
+    price_lg: 8,
+    coffee_type: "cold",
+    img_url:
+      "https://zonacooks.com/wp-content/uploads/2020/07/Banana-Mocha-Frappuccino-4-720x720.jpg",
+  },
+  {
+    name: "Hot Chocolate",
+    price_sm: 2,
+    price_md: 3,
+    price_lg: 4,
+    coffee_type: "hot",
+    img_url:
+      "https://www.yummyhealthyeasy.com/wp-content/uploads/2015/12/hot-chocolate-for-one-title-picture.jpg",
+  },
+];
+db.sequelize
+  .sync({ force: true })
+  .then(() => {
+    db.CoffeeDrinks.bulkCreate(seedData)
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  })
+  .catch((err) => console.log(err));
--- server.js
+++ server.js
@@ -2,30 +2,37 @@
 // Server.js - This file is the initial starting point for the Node/Express server.
 // *********************************************************************************
 
-// Dependencies
-// =============================================================
 var express = require("express");
 
-// Sets up the Express App
-// =============================================================
-var app = express();
+var db = require("./models");
+
 var PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Routes
+// require("./app/routes/api-routes.js")(app);
+// require("./app/routes/html-routes.js")(app);
+
+// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Static directory
-app.use(express.static("app/public"));
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
-// Routes
-// =============================================================
-require("./app/routes/api-routes.js")(app);
-require("./app/routes/html-routes.js")(app);
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-// Starts the server to begin listening
-// =============================================================
-db.sequelize.sync().then(() => {
+// Import routes and give the server access to them.
+var routes = require("./controllers/caffeinController.js");
+
+app.use(routes);
+
+db.sequelize.sync().then(function () {
   app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
