559080329275536d406f3596419e05ede94671f0
Sai Purciful <saipurciful@gmail.com>
2020-12-19 22:33:08 -0800

    added singin and login pages

--- public/js/caffeinHP.js
+++ public/js/caffeinHP.js
@@ -23,7 +23,6 @@ window.onscroll = function() {myFunction()};
 var navbar = document.getElementById("navbar");
 
 var sticky = navbar.offsetTop;
-
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
